82527804e55cd149936eeabd9d7684a5d567304a
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-08-17 22:51:25 +0900

    car brand sort
    interface.py / hyundaican.py / carcontroller.py / values.py
    
    santa_fe-1 -> remove
    sonata_h , optima_h -> hev rename
    sonata_2019 -> sonata19 , sonata19_hev
    hyundai genesis -> genesis
    car.kia -> kia remove
    
    genesis g70 , veloster add
    fw 2.0 add
    
    car brand sort -> genesis / hyundai / kia

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -54,9 +54,9 @@ def process_hud_alert(enabled, fingerprint, visual_alert, left_lane,
   left_lane_warning = 0
   right_lane_warning = 0
   if left_lane_depart:
-    left_lane_warning = 1 if fingerprint in [CAR.HYUNDAI_GENESIS, CAR.GENESIS_G90, CAR.GENESIS_G80] else 2
+    left_lane_warning = 1 if fingerprint in [CAR.GENESIS, CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.GENESIS_G90] else 2
   if right_lane_depart:
-    right_lane_warning = 1 if fingerprint in [CAR.HYUNDAI_GENESIS, CAR.GENESIS_G90, CAR.GENESIS_G80] else 2
+    right_lane_warning = 1 if fingerprint in [CAR.GENESIS, CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.GENESIS_G90] else 2
 
   return sys_warning, sys_state, left_lane_warning, right_lane_warning
 
@@ -121,7 +121,7 @@ class CarController():
 
     # Disable steering while turning blinker on and speed below 60 kph
     if CS.out.leftBlinker or CS.out.rightBlinker:
-      if self.car_fingerprint not in [CAR.KIA_OPTIMA, CAR.KIA_OPTIMA_H]:
+      if self.car_fingerprint not in [CAR.OPTIMA, CAR.OPTIMA_HEV]:
         self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
       elif CS.left_blinker_flash or CS.right_blinker_flash: # Optima has blinker flash signal only
         self.turning_signal_timer = 100
